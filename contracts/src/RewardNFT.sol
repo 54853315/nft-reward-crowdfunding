92c11d05f6ebf87e7221528e7695f67589eea5e1
konakona-xiong <konakona.xiong@gmail.com>
2025-12-04 16:29:22 +0800

    feat(nft): 更新`RewardNFT`合约以支持URI存储和安全铸造
    * 将合约从`ERC721`更改为`ERC721URIStorage`以支持URI存储
    * 使用`_safeMint`替代`_mint`以增强安全性
    * 添加`NFTMinted`事件以记录铸造操作
    * 修复`tokenURI`和`tierOf`函数中的所有者验证逻辑

--- contracts/src/RewardNFT.sol
+++ contracts/src/RewardNFT.sol
@@ -1,23 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 // import "erc721a/contracts/ERC721A.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract RewardNFT is ERC721, Ownable {
+contract RewardNFT is ERC721URIStorage, Ownable {
     string[3] baseURI = ["ipfs://bronze/", "ipfs://silver/", "ipfs://gold/"];
     uint256 private _nextTokenId;
 
     // 存储每个 tokenId 对应的 tier
     mapping(uint256 => uint256) private _tokenTiers;
 
+    event NFTMinted(uint256 indexed tokenId, address indexed user1, uint256 timestamp);
+
     constructor() ERC721("CrowdBadge", "BADGE") Ownable(msg.sender) {}
 
     function mint(address to, uint tier) external onlyOwner returns (uint256) {
+        require(to != address(0), "Invalid address");
         require(tier < 3, "Invalid tier");
         uint256 tokenId = _nextTokenId++;
-        _mint(to, tokenId);
+        _safeMint(to, tokenId);
         _tokenTiers[tokenId] = tier;
+        emit NFTMinted(tokenId, to, block.timestamp);
         return tokenId;
     }
 
@@ -26,12 +29,12 @@ contract RewardNFT is ERC721, Ownable {
     }
 
     function tokenURI(uint tokenId) public view override returns (string memory) {
-        require(ownerOf(tokenId) != address(0), "Token does not exist");
+        require(_ownerOf(tokenId) != address(0), "Token does not exist");
         return string(abi.encodePacked(baseURI[_tokenTiers[tokenId]]));
     }
 
     function tierOf(uint256 tokenId) public view returns (uint256) {
-        require(ownerOf(tokenId) != address(0), "Token does not exist");
+        require(_ownerOf(tokenId) != address(0), "Token does not exist");
         return _tokenTiers[tokenId];
     }
 }
